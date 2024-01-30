d1ec0c6a2104485bbbe606d7ae64ec98e5fe3e9c
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 23:31:28 +0300

    Add project files.

---
+++ TcKimlikKontrol/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TcKimlikKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tckimlik = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_soyisim = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_dogum_tarihi = new System.Windows.Forms.TextBox();
+            this.btn_kaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(117, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TCKİMLİK";
+            // 
+            // txt_tckimlik
+            // 
+            this.txt_tckimlik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_tckimlik.Location = new System.Drawing.Point(294, 62);
+            this.txt_tckimlik.Name = "txt_tckimlik";
+            this.txt_tckimlik.Size = new System.Drawing.Size(130, 26);
+            this.txt_tckimlik.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(117, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İSİM";
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_isim.Location = new System.Drawing.Point(294, 120);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(130, 26);
+            this.txt_isim.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(117, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SOYİSİM";
+            // 
+            // txt_soyisim
+            // 
+            this.txt_soyisim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_soyisim.Location = new System.Drawing.Point(294, 182);
+            this.txt_soyisim.Name = "txt_soyisim";
+            this.txt_soyisim.Size = new System.Drawing.Size(130, 26);
+            this.txt_soyisim.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(117, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "DOĞUM TARİHİ";
+            // 
+            // txt_dogum_tarihi
+            // 
+            this.txt_dogum_tarihi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_dogum_tarihi.Location = new System.Drawing.Point(294, 245);
+            this.txt_dogum_tarihi.Name = "txt_dogum_tarihi";
+            this.txt_dogum_tarihi.Size = new System.Drawing.Size(130, 26);
+            this.txt_dogum_tarihi.TabIndex = 2;
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kaydet.Location = new System.Drawing.Point(171, 345);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(149, 44);
+            this.btn_kaydet.TabIndex = 3;
+            this.btn_kaydet.Text = "KAYDET";
+            this.btn_kaydet.UseVisualStyleBackColor = true;
+            this.btn_kaydet.Click += new System.EventHandler(this.btn_kaydet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_kaydet);
+            this.Controls.Add(this.txt_dogum_tarihi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_soyisim);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_isim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_tckimlik);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_tckimlik;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_soyisim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_dogum_tarihi;
+        private System.Windows.Forms.Button btn_kaydet;
+    }
+}
+
---
+++ TcKimlikKontrol/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcKimlikKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_kaydet_Click(object sender, EventArgs e)
+        {
+            tcno.KPSPublicSoapClient t = new tcno.KPSPublicSoapClient();
+           bool answer = t.TCKimlikNoDogrula(Convert.ToInt64(txt_tckimlik.Text), txt_isim.Text.ToUpper(), txt_soyisim.Text.ToUpper(), Convert.ToInt32(txt_dogum_tarihi.Text));
+
+            if (answer)
+            {
+                MessageBox.Show("BİLGİLER DOĞRU");
+            }
+            else
+            {
+                MessageBox.Show("İsim, Soyisim, Doğumyılı ve Tc Kimlik alanlarını tekrar kontrol ediniz...");
+            }
+        }
+    }
+}
